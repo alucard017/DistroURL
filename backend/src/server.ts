90bf4a2ada9444513aac7d329a64615fda0430b2
Apurba Sundar Nayak <apurbasundar2002@gmail.com>
2025-06-09 20:26:19 +0530

    modified distroURL

--- backend/src/server.ts
+++
@@ -1,8 +0,0 @@
-import app from "./app";
-import config from "./config/index";
-
-const { ServerConfig } = config;
-
-app.listen(ServerConfig.PORT, () => {
-  console.log(`listening on ${ServerConfig.PORT}`);
-});
--- backend/src/utils/ValidateURL.ts
+++
@@ -1,5 +0,0 @@
-const isValidUrl = (url: string): boolean => {
-  const regex = /^(ftp|http|https):\/\/[^ "]+$/;
-  return regex.test(url);
-};
-export default isValidUrl;
